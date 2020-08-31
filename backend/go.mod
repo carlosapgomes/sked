9b5d126c25e3f39cbde2aedacfe422297f4287e0
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-08-30 21:41:47 -0300

    run tests against a temporary postgres container

--- backend/go.mod
+++ backend/go.mod
@@ -3,14 +3,21 @@ module carlosapgomes.com/sked
 go 1.13
 
 require (
+	github.com/cenkalti/backoff/v3 v3.2.2 // indirect
+	github.com/containerd/continuity v0.0.0-20200710164510-efbc4488d8fe // indirect
 	github.com/golang/gddo v0.0.0-20190904175337-72a348e765d2
 	github.com/google/go-cmp v0.3.1 // indirect
 	github.com/lib/pq v1.2.0
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
-	github.com/pkg/errors v0.8.1
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/ory/dockertest/v3 v3.6.0
+	github.com/pkg/errors v0.9.1
 	github.com/satori/go.uuid v1.2.0
 	github.com/sendgrid/rest v2.4.1+incompatible // indirect
 	github.com/sendgrid/sendgrid-go v3.5.0+incompatible
-	golang.org/x/crypto v0.0.0-20191122220453-ac88ee75c92c
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sys v0.0.0-20200828194041-157a740278f4 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 )
--- backend/internal/storage/testutils_test.go
+++ backend/internal/storage/testutils_test.go
@@ -3,17 +3,115 @@ package storage_test
 import (
 	"database/sql"
 	"io/ioutil"
+	"log"
+	"net"
+	"net/url"
+	"runtime"
 	"testing"
+	"time"
+
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
 	// // blank inport posgres lib
 	// _ "github.com/lib/pq"
 )
+
+// Create a temporary postgres container
+// source:  https://jbrandhorst.com/post/postgres
+// https://github.com/johanbrandhorst/grpc-postgres
+func startDatabase(tb testing.TB) *url.URL {
+	tb.Helper()
+
+	pgURL := &url.URL{
+		Scheme: "postgres",
+		User:   url.UserPassword("test_sked", "test_sked"),
+		Path:   "test_sked",
+	}
+	q := pgURL.Query()
+	q.Add("sslmode", "disable")
+	pgURL.RawQuery = q.Encode()
+
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		tb.Fatalf("Could not connect to docker: %v", err)
+	}
+
+	pw, _ := pgURL.User.Password()
+	env := []string{
+		"POSTGRES_USER=" + pgURL.User.Username(),
+		"POSTGRES_PASSWORD=" + pw,
+		"POSTGRES_DB=" + pgURL.Path,
+	}
+
+	resource, err := pool.Run("postgres", "13-alpine", env)
+	if err != nil {
+		tb.Fatalf("Could not start postgres container: %v", err)
+	}
+	tb.Cleanup(func() {
+		err = pool.Purge(resource)
+		if err != nil {
+			tb.Fatalf("Could not purge container: %v", err)
+		}
+	})
+
+	pgURL.Host = resource.Container.NetworkSettings.IPAddress
+
+	// Docker layer network is different on Mac
+	if runtime.GOOS == "darwin" {
+		pgURL.Host = net.JoinHostPort(resource.GetBoundIP("5432/tcp"), resource.GetPort("5432/tcp"))
+	}
+
+	logWaiter, err := pool.Client.AttachToContainerNonBlocking(docker.AttachToContainerOptions{
+		Container:    resource.Container.ID,
+		OutputStream: log.Writer(),
+		ErrorStream:  log.Writer(),
+		Stderr:       true,
+		Stdout:       true,
+		Stream:       true,
+	})
+	if err != nil {
+		tb.Fatalf("Could not connect to postgres container log output: %v", err)
+	}
+
+	tb.Cleanup(func() {
+		err = logWaiter.Close()
+		if err != nil {
+			tb.Fatalf("Could not close container log: %v", err)
+		}
+		err = logWaiter.Wait()
+		if err != nil {
+			tb.Fatalf("Could not wait for container log to close: %v", err)
+		}
+	})
+
+	pool.MaxWait = 10 * time.Second
+	err = pool.Retry(func() (err error) {
+		db, err := sql.Open("postgres", pgURL.String())
+		if err != nil {
+			return err
+		}
+		defer func() {
+			cerr := db.Close()
+			if err == nil {
+				err = cerr
+			}
+		}()
+
+		return db.Ping()
+	})
+	if err != nil {
+		tb.Fatalf("Could not connect to postgres container: %v", err)
+	}
+
+	return pgURL
+}
 
 // Shamelessly copied from "Let'sGo!" book
 // by Alex Edwards (https://www.alexedwards.net/#book)
 // Establish a sql.DB connection pool for our test database.
 func newTestDB(t *testing.T) (*sql.DB, func()) {
 	// Initialise a new connection pool
-	db, err := sql.Open("postgres", "postgres://test_sked:test_sked@localhost/test_sked?sslmode=disable")
+	db, err := sql.Open("postgres", startDatabase(t).String())
 	if err != nil {
 		t.Fatal(err)
 	}
