80ce89ce36efbf35b46411b7a1b17d997ccbd052
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-09-15 10:21:43 -0300

    handlers are not exported

--- backend/internal/web/patientHandlers.go
+++ backend/internal/web/patientHandlers.go
@@ -5,42 +5,42 @@ import (
 )
 
 // CreatePatient creates a new patient record
-func (app App) CreatePatient() http.Handler {
+func (app App) createPatient() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
 	})
 }
 
 // FindPatientByID
-func (app App) FindPatientByID() http.Handler {
+func (app App) findPatientByID() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
 	})
 }
 
 // UpdatePatientName
-func (app App) UpdatePatientName() http.Handler {
+func (app App) updatePatientName() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
 	})
 }
 
 // UpdatePatientPhone
-func (app App) UpdatePatientPhone() http.Handler {
+func (app App) updatePatientPhone() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
 	})
 }
 
 // GetAllPatients
-func (app App) GetAllPatients() http.Handler {
+func (app App) getAllPatients() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
 	})
 }
 
 // FindPatientByName
-func (app App) FindPatientByName() http.Handler {
+func (app App) findPatientByName() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
 	})
