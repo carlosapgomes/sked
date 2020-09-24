1788343d16f1c114e106d6b295e37cfbe7efa73c
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-09-24 19:18:05 -0300

    add passing integration test for get patient name endpoint

--- backend/internal/web/patientHandlers_test.go
+++ backend/internal/web/patientHandlers_test.go
@@ -78,7 +78,70 @@ func TestFindPatientByName(t *testing.T) {
 		})
 	}
 }
+func TestGetPatientName(t *testing.T) {
+	handlers := web.New(
+		log.New(ioutil.Discard, "", 0),
+		log.New(ioutil.Discard, "", 0),
+		&web.CkProps{
+			Name:     "sid",
+			HTTPOnly: false,
+			Secure:   false,
+		},
+		mocks.NewSessionSvc(),
+		services.NewUserService(mocks.NewUserRepo()),
+		nil,
+		mocks.NewTokenMockSvc(),
+		services.NewPatientService(mocks.NewPatientRepo()),
+		nil,
+		nil,
+	)
+	ts := newTestServer(t, handlers.Routes())
+	defer ts.Close()
+	var tests = []struct {
+		name     string
+		searchID string
+		wantBody []byte
+		wantCode int
+	}{
+		{"Valid Search", "85f45ff9-d31c-4ff7-94ac-5afb5a1f0fcd", []byte("Valid Patient"), http.StatusOK},
+		{"Valid Search But Missing Patient", "2e134760-2006-4dc7-a315-025dc1081fb0", nil, http.StatusInternalServerError},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			path := ts.URL + "/patients/" + tt.searchID + "/name"
+			req, _ := http.NewRequest(http.MethodGet, path, nil)
+			cookie := &http.Cookie{
+				Name:  "sid",
+				Value: "167ced64-af16-45d2-bb08-e35233c04ad1",
+			}
+			req.AddCookie(cookie)
+			rs, err := ts.Client().Do(req)
+			if err != nil {
+				t.Fatal(err)
+			}
+			code := rs.StatusCode
+			if code != tt.wantCode {
+				t.Errorf("want %d; got %d", tt.wantCode, code)
+			}
+			var response patient.Patient
+			if tt.wantCode == http.StatusOK {
+				defer rs.Body.Close()
+				respBody, _ := ioutil.ReadAll(rs.Body)
+				//t.Logf("%s\n", respBody)
+				err = json.Unmarshal(respBody, &response)
+				if err != nil {
+					t.Error("bad response body")
+				}
+			}
+			if tt.wantBody != nil {
+				if !bytes.Contains([]byte(response.Name), tt.wantBody) {
+					t.Errorf("want body %s to contain %q", response, tt.wantBody)
+				}
+			}
+		})
+	}
 
+}
 func TestFindPatientByID(t *testing.T) {
 	handlers := web.New(
 		log.New(ioutil.Discard, "", 0),
