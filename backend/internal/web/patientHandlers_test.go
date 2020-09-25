14fd211bb78badd0b9cd9c4ac0cbd2cb261e3b63
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-09-24 21:07:52 -0300

    add passing integration test for update patient phone endpoint

--- backend/internal/web/patientHandlers_test.go
+++ backend/internal/web/patientHandlers_test.go
@@ -298,6 +298,94 @@ func TestUpdatePatientName(t *testing.T) {
 	}
 
 }
+func TestUpdatePatientPhones(t *testing.T) {
+	handlers := web.New(
+		log.New(ioutil.Discard, "", 0),
+		log.New(ioutil.Discard, "", 0),
+		&web.CkProps{
+			Name:     "sid",
+			HTTPOnly: false,
+			Secure:   false,
+		},
+		mocks.NewSessionSvc(),
+		services.NewUserService(mocks.NewUserRepo()),
+		nil,
+		mocks.NewTokenMockSvc(),
+		services.NewPatientService(mocks.NewPatientRepo()),
+		nil,
+		nil,
+	)
+	ts := newTestServer(t, handlers.Routes())
+	defer ts.Close()
+	var tests = []struct {
+		name      string
+		patientID string
+		newPhones []string
+		wantCode  int
+	}{
+		{"Valid Update", "85f45ff9-d31c-4ff7-94ac-5afb5a1f0fcd", []string{"4321567"}, http.StatusOK},
+		{"Invalid Patient ID", "2e134760-2006-4dc7-a315-025dc1081fb0", nil, http.StatusInternalServerError},
+	}
+	type postBody struct {
+		Phones []string `json:"phones"`
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			path := ts.URL + "/patients/" + tt.patientID + "/phones"
+			reqBody := &postBody{Phones: tt.newPhones}
+			body, err := json.Marshal(reqBody)
+			if err != nil {
+				t.Log(err)
+			}
+			req, _ := http.NewRequest(http.MethodPut, path, strings.NewReader(string(body)))
+			cookie := &http.Cookie{
+				Name:  "sid",
+				Value: "167ced64-af16-45d2-bb08-e35233c04ad1",
+			}
+			req.AddCookie(cookie)
+			rs, err := ts.Client().Do(req)
+			if err != nil {
+				t.Fatal(err)
+			}
+			code := rs.StatusCode
+			if code != tt.wantCode {
+				t.Errorf("want %d; got %d", tt.wantCode, code)
+			}
+			if tt.wantCode == http.StatusOK {
+				path = ts.URL + "/patients/" + tt.patientID + "/phones"
+				req, _ = http.NewRequest(http.MethodGet, path, nil)
+				req.AddCookie(cookie)
+				rs, err = ts.Client().Do(req)
+				if err != nil {
+					t.Fatal(err)
+				}
+				var response struct {
+					Phones []string
+				}
+				if rs.StatusCode == http.StatusOK {
+					defer rs.Body.Close()
+					respBody, _ := ioutil.ReadAll(rs.Body)
+					t.Logf("%s\n", respBody)
+					err = json.Unmarshal(respBody, &response)
+					if err != nil {
+						t.Error("bad response body")
+					}
+					contains := false
+					for _, e := range response.Phones {
+						if bytes.Contains([]byte(e), []byte(tt.newPhones[0])) {
+							contains = true
+						}
+					}
+					if !contains {
+						t.Errorf("want body %s to contain %q", response, tt.newPhones)
+					}
+				}
+			}
+
+		})
+	}
+
+}
 func TestFindPatientByID(t *testing.T) {
 	handlers := web.New(
 		log.New(ioutil.Discard, "", 0),
