8ee0830eb778ca88a9192f1eaab647ac14a1a694
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-10-01 20:35:46 -0300

    insert test appointments

---
+++ backend/internal/storage/testdata/insert_appointments.sql
@@ -0,0 +1,91 @@
+-- insert test appointments
+
+INSERT INTO appointments (id, date_time, patient_name, patient_id, doctor_name,
+doctor_id, notes, canceled, completed, created_by, created_at, updated_by,
+updated_at) VALUES (
+'5e6f7cd1-d8d2-40cd-97a3-aca01a93bfde',
+'2020-09-07 12:00:00 UTC',
+'John Doe',
+'22070f56-5d52-43f0-9f59-5de61c1db506',
+'Dr House',
+'f06244b9-97e5-4f1a-bae0-3b6da7a0b604',
+'some notes',
+FALSE,
+FALSE,
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC',
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC'
+);
+
+
+INSERT INTO appointments (id, date_time, patient_name, patient_id, doctor_name,
+doctor_id, notes, canceled, completed, created_by, created_at, updated_by,
+updated_at) VALUES (
+'7fef3c47-a01a-42a6-ac45-27a440596751',
+'2020-09-08 12:00:00 UTC',
+'John Doe',
+'22070f56-5d52-43f0-9f59-5de61c1db506',
+'Dr House',
+'f06244b9-97e5-4f1a-bae0-3b6da7a0b604',
+'some notes',
+FALSE,
+FALSE,
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC',
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC'
+);	
+
+INSERT INTO appointments (id, date_time, patient_name, patient_id, doctor_name,
+doctor_id, notes, canceled, completed, created_by, created_at, updated_by,
+updated_at) VALUES (
+'19f66dc6-b5c8-497b-bba2-b982bb85ded8',
+'2020-09-09 12:00:00 UTC',
+'John Doe',
+'22070f56-5d52-43f0-9f59-5de61c1db506',
+'Dr House',
+'f06244b9-97e5-4f1a-bae0-3b6da7a0b604',
+'some notes',
+FALSE,
+FALSE,
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-08 12:00:00 UTC'
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-08 12:00:00 UTC'
+);
+
+
+INSERT INTO appointments (id, date_time, patient_name, patient_id, doctor_name,
+doctor_id, notes, canceled, completed, created_by, created_at, updated_by,
+updated_at) VALUES (
+'723e2fa0-70a9-4c20-89d9-b5f69405b772',
+'2020-09-06 12:00:00 UTC',
+'John Doe',
+'22070f56-5d52-43f0-9f59-5de61c1db506',
+'Dr House',
+'f06244b9-97e5-4f1a-bae0-3b6da7a0b604',
+'some notes',
+FALSE,
+FALSE,
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC',
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC'
+);
+	
+INSERT INTO appointments (id, date_time, patient_name, patient_id, doctor_name,
+doctor_id, notes, canceled, completed, created_by, created_at, updated_by,
+updated_at) VALUES (
+'5792340a-8c35-4183-a388-2459a8e0295a',
+'2020-09-11 12:00:00 UTC',
+'John Doe',
+'22070f56-5d52-43f0-9f59-5de61c1db506',
+'Dr House',
+'f06244b9-97e5-4f1a-bae0-3b6da7a0b604',
+'some notes',
+FALSE,
+FALSE,
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC'
+);
