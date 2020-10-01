ab630086a9cc07ea652b47b6e5692382a652c525
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-10-01 19:53:52 -0300

    split tests data setup sql file

---
+++ backend/internal/storage/testdata/insert_sessions.sql
@@ -0,0 +1,16 @@
+--insert test sessions
+
+INSERT INTO sessions (id, userid, created_at, expires_at) VALUES(
+'144f1223-70f4-4fca-9c99-c58eed7a0f4a',
+'dcce1beb-aee6-4a4d-b724-94d470817323',
+'2019-06-23 17:00:00 UTC',
+'2019-06-23 17:00:00 UTC'
+);
+
+INSERT INTO sessions (id, userid, created_at, expires_at) VALUES(
+'ddfd72b2-d57b-4fc4-9265-55b9dae7287c',
+'dcce1beb-aee6-4a4d-b724-94d470817323',
+'2019-06-23 18:25:22 UTC',
+'2019-06-23 18:45:22 UTC'
+);
+
