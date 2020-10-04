bfd05089e7f3d8ea67bd5e3d8b1060e161854608
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-10-04 08:53:50 -0300

    unify database setup both for prod and for testing

--- backend/internal/storage/_dbsetup.sql
+++ backend/internal/storage/_dbsetup.sql
@@ -1,16 +1,16 @@
-/* This is the setup script for Gobackend Database
- run this script with the following command:
+/* This is the databse setup script 
+ execute it with the following command:
  psql -U sked -d sked -a -f _dbsetup.sql
 */
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id UUID NOT NULL PRIMARY KEY UNIQUE,
-  name TEXT,
+  name TEXT NOT NULL,
   email TEXT NOT NULL UNIQUE,
-  phone TEXT,
-  hashedpw CHAR(60) NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE,
-  updated_at TIMESTAMP WITH TIME ZONE,
+  phone TEXT NOT NULL,
+  hashedpw TEXT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   active BOOL NOT NULL DEFAULT FALSE,
   email_was_validated BOOL NOT NULL DEFAULT FALSE,
   roles TEXT[]
@@ -21,70 +21,69 @@ CREATE INDEX users_email_index ON users (email);
 DROP TABLE IF EXISTS patients;
 CREATE TABLE patients (
   id UUID NOT NULL PRIMARY KEY UNIQUE,
-  name TEXT,
+  name TEXT NOT NULL,
   address TEXT,
   city TEXT,
   state CHAR(2),
-  phones TEXT[],
-  created_by UUID,
-  created_at TIMESTAMP WITH TIME ZONE,
-  updated_by UUID,
-  updated_at TIMESTAMP WITH TIME ZONE
+  phones TEXT[] NOT NULL,
+  created_by UUID NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  updated_by UUID NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
-
 CREATE INDEX patients_id_index ON patients (id);
 CREATE INDEX patients_name_index ON patients (name);
 
 DROP TABLE IF EXISTS appointments;
 CREATE TABLE appointments (
   id UUID NOT NULL PRIMARY KEY UNIQUE,
-  date_time TIMESTAMP WITH TIME ZONE,
-  patient_name TEXT,
-  patient_id UUID,
-  doctor_name TEXT,
-  doctor_id UUID,
+  date_time TIMESTAMP WITH TIME ZONE NOT NULL,
+  patient_name TEXT NOT NULL,
+  patient_id UUID NOT NULL,
+  doctor_name TEXT NOT NULL,
+  doctor_id UUID NOT NULL,
   notes TEXT,
-  canceled BOOL,
-  completed BOOL,
-  created_by UUID,
-  created_at TIMESTAMP WITH TIME ZONE,
-  updated_by UUID,
-  updated_at TIMESTAMP WITH TIME ZONE
+  canceled BOOL NOT NULL DEFAULT FALSE,
+  completed BOOL NOT NULL DEFAULT FALSE,
+  created_by UUID NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  updated_by UUID NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 )
 CREATE INDEX appointments_id_index ON appointments (id);
 CREATE INDEX appointments_patient_id_index ON appointments (patient_id);
 CREATE INDEX appointments_doctor_id_index ON appointments (doctor_id);
-CREATE INDEX appointments_date_time_index ON appointments (date_time);
+CREATE INDEX appointments_date_index ON appointments (date(date_time));
 
 DROP TABLE IF EXISTS surgeries;
 CREATE TABLE surgeries (
   id UUID NOT NULL PRIMARY KEY UNIQUE,
-  date_time TIMESTAMP WITH TIME ZONE,
-  patient_name TEXT,
-  patient_id UUID,
-  doctor_name TEXT,
-  doctor_id UUID,
+  date_time TIMESTAMP WITH TIME ZONE NOT NULL,
+  patient_name TEXT NOT NULL,
+  patient_id UUID NOT NULL,
+  doctor_name TEXT NOT NULL,
+  doctor_id UUID NOT NULL,
   notes TEXT,
-  proposed_surgery TEXT,
-  canceled BOOL,
-  done BOOL,
-  created_by UUID,
-  created_at TIMESTAMP WITH TIME ZONE,
-  updated_by UUID,
-  updated_at TIMESTAMP WITH TIME ZONE
+  proposed_surgery TEXT NOT NULL,
+  canceled BOOL DEFAULT FALSE,
+  done BOOL DEFAULT FALSE,
+  created_by UUID NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  updated_by UUID NOT NULL,
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 )
 CREATE INDEX surgeries_id_index ON surgeries (id);
 CREATE INDEX surgeries_patient_id_index ON surgeries (patient_id);
 CREATE INDEX surgeries_doctor_id_index ON surgeries (doctor_id);
-CREATE INDEX surgeries_date_time_index ON surgeries (date_time);
+CREATE INDEX surgeries_date_time_index ON surgeries (date(date_time));
 
 
 DROP TABLE IF EXISTS sessions;
 CREATE TABLE sessions (
   id UUID NOT NULL PRIMARY KEY UNIQUE,
   userid UUID NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE,
-  expires_at TIMESTAMP WITH TIME ZONE
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  expires_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
 CREATE INDEX sessions_id_index ON sessions (id);
@@ -94,8 +93,8 @@ DROP TABLE IF EXISTS tokens;
 CREATE TABLE tokens (
   id TEXT NOT NULL PRIMARY KEY UNIQUE CHECK (id <> ''),
   userid UUID NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE,
-  expires_at TIMESTAMP WITH TIME ZONE,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
   kind TEXT
 );
 
--- backend/internal/storage/testdata/setup.sql
+++ backend/internal/storage/testdata/setup.sql
@@ -1,78 +1,94 @@
-CREATE TABLE users (
-  id UUID NOT NULL PRIMARY KEY UNIQUE,
-  name TEXT,
-  email TEXT NOT NULL UNIQUE,
-  phone TEXT,
-  hashedpw TEXT NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE,
-  updated_at TIMESTAMP WITH TIME ZONE,
-  active BOOL NOT NULL DEFAULT FALSE,
-  email_was_validated BOOL NOT NULL DEFAULT FALSE,
-  roles TEXT[]
-);
+-- CREATE TABLE users (
+  -- id UUID NOT NULL PRIMARY KEY UNIQUE,
+  -- name TEXT,
+  -- email TEXT NOT NULL UNIQUE,
+  -- phone TEXT,
+  -- hashedpw TEXT NOT NULL,
+  -- created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  -- updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  -- active BOOL NOT NULL DEFAULT FALSE,
+  -- email_was_validated BOOL NOT NULL DEFAULT FALSE,
+  -- roles TEXT[]
+-- );
+-- CREATE INDEX users_id_index ON users (id);
+-- CREATE INDEX users_email_index ON users (email);
 
 
-CREATE TABLE patients (
-  id UUID NOT NULL PRIMARY KEY UNIQUE,
-  name VARCHAR(100),
-  address VARCHAR(150),
-  city VARCHAR(100),
-  state CHAR(2),
-  phones TEXT[],
-  created_by UUID NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE,
-  updated_by UUID NOT NULL,
-  updated_at TIMESTAMP WITH TIME ZONE
-);
+-- CREATE TABLE patients (
+  -- id UUID NOT NULL PRIMARY KEY UNIQUE,
+  -- name TEXT,
+  -- address TEXT, 
+  -- city TEXT, 
+  -- state CHAR(2),
+  -- phones TEXT[],
+  -- created_by UUID NOT NULL,
+  -- created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  -- updated_by UUID NOT NULL,
+  -- updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+-- );
+-- CREATE INDEX patients_id_index ON patients (id);
+-- CREATE INDEX patients_name_index ON patients (name);
 
-CREATE TABLE appointments{
-    id UUID NOT NULL PRIMARY KEY UNIQUE,
-    date_time TIMESTAMPWITH TIME ZONE,
-    patient_name VARCHAR(100),
-    patient_id UUID NOT NULL, 
-    doctor_name VARCHAR(100),
-    doctor_id UUID NOT NULL,
-    notes TEXT,
-    canceled BOOL,
-    completed BOOL,
-    created_by UUID NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE,
-    updated_by UUID NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE
-};
+-- CREATE TABLE appointments{
+    -- id UUID NOT NULL PRIMARY KEY UNIQUE,
+    -- date_time TIMESTAMP WITH TIME ZONE NOT NULL,
+    -- patient_name TEXT NOT NULL, 
+    -- patient_id UUID NOT NULL, 
+    -- doctor_name TEXT NOT NULL, 
+    -- doctor_id UUID NOT NULL,
+    -- notes TEXT,
+    -- canceled BOOL NOT NULL DEFAULT FALSE,
+    -- completed BOOL NOT NULL DEFAULT FALSE,
+    -- created_by UUID NOT NULL,
+    -- created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    -- updated_by UUID NOT NULL,
+    -- updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+-- };
+-- CREATE INDEX appointments_id_index ON appointments (id);
+-- CREATE INDEX appointments_patient_id_index ON appointments (patient_id);
+-- CREATE INDEX appointments_doctor_id_index ON appointments (doctor_id);
+-- CREATE INDEX appointments_date_index ON appointments (date(date_time));
 
-CREATE TABLE surgeries{
-    id UUID NOT NULL PRIMARY KEY UNIQUE,
-    date_time TIMESTAMPWITH TIME ZONE,
-    patient_name VARCHAR(100),
-    patient_id UUID NOT NULL, 
-    doctor_name VARCHAR(100),
-    doctor_id UUID NOT NULL,
-    notes TEXT,
-    proposed_surgery TEXT,
-    canceled BOOL,
-    done BOOL,
-    created_by UUID NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE,
-    updated_by UUID NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE
-};
+-- CREATE TABLE surgeries{
+    -- id UUID NOT NULL PRIMARY KEY UNIQUE,
+    -- date_time TIMESTAMP WITH TIME ZONE NOT NULL,
+    -- patient_name TEXT NOT NULL, 
+    -- patient_id UUID NOT NULL, 
+    -- doctor_name TEXT NOT NULL, 
+    -- doctor_id UUID NOT NULL,
+    -- notes TEXT,
+    -- proposed_surgery TEXT NOT NULL,
+    -- canceled BOOL DEFAULT FALSE,
+    -- done BOOL DEFAULT FALSE,
+    -- created_by UUID NOT NULL,
+    -- created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    -- updated_by UUID NOT NULL,
+    -- updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+-- };
+-- CREATE INDEX surgeries_id_index ON surgeries (id);
+-- CREATE INDEX surgeries_patient_id_index ON surgeries (patient_id);
+-- CREATE INDEX surgeries_doctor_id_index ON surgeries (doctor_id);
+-- CREATE INDEX surgeries_date_index ON surgeries (date(date_time));
 
-CREATE TABLE sessions (
-  id UUID NOT NULL PRIMARY KEY UNIQUE,
-  userid UUID NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE,
-  expires_at TIMESTAMP WITH TIME ZONE
-);
+-- CREATE TABLE sessions (
+  -- id UUID NOT NULL PRIMARY KEY UNIQUE,
+  -- userid UUID NOT NULL,
+  -- created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  -- expires_at TIMESTAMP WITH TIME ZONE NOT NULL
+-- );
+-- CREATE INDEX sessions_id_index ON sessions (id);
+-- CREATE INDEX sessions_userid_index ON sessions (userid);
 
-CREATE TABLE tokens (
-  id TEXT NOT NULL PRIMARY KEY UNIQUE CHECK (id <> ''),
-  userid UUID NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE,
-  expires_at TIMESTAMP WITH TIME ZONE,
-  kind TEXT
-);
-
+-- CREATE TABLE tokens (
+  -- id TEXT NOT NULL PRIMARY KEY UNIQUE CHECK (id <> ''),
+  -- userid UUID NOT NULL,
+  -- created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  -- expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  -- kind TEXT
+-- );
+-- CREATE INDEX tokens_id_index ON tokens (id);
+-- CREATE INDEX tokens_userid_index ON tokens (userid);
+\i ../_dbsetup.sql
 \i insert_tokes.sql
 \i insert_sessions.sql
 \i insert_users.sql
