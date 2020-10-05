1b48829605cf40f74cdd5edf799ce74aff86ab25
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-10-05 20:34:09 -0300

    more tests for appointments repository

--- backend/internal/storage/appointments_test.go
+++ backend/internal/storage/appointments_test.go
@@ -142,3 +142,139 @@ func TestFindAppointmentByID(t *testing.T) {
 	}
 
 }
+
+func TestFindAppointmentsByPatientID(t *testing.T) {
+	var tests = []struct {
+		name      string
+		appointmt appointment.Appointment
+		wantSize  int
+		wantError error
+	}{
+		{"Valid Update",
+			appointment.Appointment{
+				ID:          "5e6f7cd1-d8d2-40cd-97a3-aca01a93bfde",
+				DateTime:    time.Date(2020, 9, 7, 12, 0, 0, 0, time.UTC),
+				PatientName: "John Doe",
+				PatientID:   "22070f56-5d52-43f0-9f59-5de61c1db506",
+				DoctorName:  "Dr House",
+				DoctorID:    "f06244b9-97e5-4f1a-bae0-3b6da7a0b604",
+				Notes:       "some notes",
+				Canceled:    false,
+				Completed:   false,
+				CreatedBy:   "10b9ad06-e86d-4a85-acb1-d7e268d1f21a",
+				CreatedAt:   time.Date(2020, 9, 6, 12, 0, 0, 0, time.UTC),
+				UpdatedBy:   "10b9ad06-e86d-4a85-acb1-d7e268d1f21a",
+				UpdatedAt:   time.Date(2020, 9, 6, 12, 0, 0, 0, time.UTC),
+			},
+			5,
+			nil,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			db, teardown := newTestDB(t)
+			defer teardown()
+			repo := storage.NewPgAppointmentRepository(db)
+			appointmts, err := repo.FindByPatientID(tt.appointmt.PatientID)
+			if !errors.Is(err, tt.wantError) {
+				t.Errorf("want %v; got %v", tt.wantError, err)
+			}
+			if len(appointmts) != tt.wantSize {
+				t.Errorf("want answer size of %d; got %d\n",
+					tt.wantSize, len(appointmts))
+			}
+		})
+	}
+}
+
+func TestFindAppointmentsByDoctorID(t *testing.T) {
+	var tests = []struct {
+		name      string
+		appointmt appointment.Appointment
+		wantSize  int
+		wantError error
+	}{
+		{"Valid Update",
+			appointment.Appointment{
+				ID:          "5e6f7cd1-d8d2-40cd-97a3-aca01a93bfde",
+				DateTime:    time.Date(2020, 9, 7, 12, 0, 0, 0, time.UTC),
+				PatientName: "John Doe",
+				PatientID:   "22070f56-5d52-43f0-9f59-5de61c1db506",
+				DoctorName:  "Dr House",
+				DoctorID:    "f06244b9-97e5-4f1a-bae0-3b6da7a0b604",
+				Notes:       "some notes",
+				Canceled:    false,
+				Completed:   false,
+				CreatedBy:   "10b9ad06-e86d-4a85-acb1-d7e268d1f21a",
+				CreatedAt:   time.Date(2020, 9, 6, 12, 0, 0, 0, time.UTC),
+				UpdatedBy:   "10b9ad06-e86d-4a85-acb1-d7e268d1f21a",
+				UpdatedAt:   time.Date(2020, 9, 6, 12, 0, 0, 0, time.UTC),
+			},
+			5,
+			nil,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			db, teardown := newTestDB(t)
+			defer teardown()
+			repo := storage.NewPgAppointmentRepository(db)
+			appointmts, err := repo.FindByDoctorID(tt.appointmt.DoctorID)
+			if !errors.Is(err, tt.wantError) {
+				t.Errorf("want %v; got %v", tt.wantError, err)
+			}
+			if len(appointmts) != tt.wantSize {
+				t.Errorf("want answer size of %d; got %d\n",
+					tt.wantSize, len(appointmts))
+			}
+		})
+	}
+}
+
+func TestFindAppointmentsByDate(t *testing.T) {
+	var tests = []struct {
+		name      string
+		appointmt appointment.Appointment
+		wantSize  int
+		wantError error
+	}{
+		{"Valid Update",
+			appointment.Appointment{
+				ID:          "5e6f7cd1-d8d2-40cd-97a3-aca01a93bfde",
+				DateTime:    time.Date(2020, 9, 7, 12, 0, 0, 0, time.UTC),
+				PatientName: "John Doe",
+				PatientID:   "22070f56-5d52-43f0-9f59-5de61c1db506",
+				DoctorName:  "Dr House",
+				DoctorID:    "f06244b9-97e5-4f1a-bae0-3b6da7a0b604",
+				Notes:       "some notes",
+				Canceled:    false,
+				Completed:   false,
+				CreatedBy:   "10b9ad06-e86d-4a85-acb1-d7e268d1f21a",
+				CreatedAt:   time.Date(2020, 9, 6, 12, 0, 0, 0, time.UTC),
+				UpdatedBy:   "10b9ad06-e86d-4a85-acb1-d7e268d1f21a",
+				UpdatedAt:   time.Date(2020, 9, 6, 12, 0, 0, 0, time.UTC),
+			},
+			1,
+			nil,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			db, teardown := newTestDB(t)
+			defer teardown()
+			repo := storage.NewPgAppointmentRepository(db)
+			appointmts, err := repo.FindByDate(tt.appointmt.DateTime)
+			if !errors.Is(err, tt.wantError) {
+				t.Errorf("want %v; got %v", tt.wantError, err)
+			}
+			if len(appointmts) != tt.wantSize {
+				t.Errorf("want answer size of %d; got %d\n",
+					tt.wantSize, len(appointmts))
+			}
+		})
+	}
+
+}
