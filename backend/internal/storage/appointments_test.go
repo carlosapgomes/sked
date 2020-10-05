b5782e0f39478a02c9a834f79436c5366c849604
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-10-05 20:13:05 -0300

    add passing tests for Appointments repository

--- backend/internal/storage/appointments_test.go
+++ backend/internal/storage/appointments_test.go
@@ -17,14 +17,19 @@ func TestCreateAppointment(t *testing.T) {
 	}{
 		{"Valid Appointment",
 			&appointment.Appointment{
-				ID:          "f80bcdf7-6a95-4595-a9ce-f411b969ab51",
+				ID:          "60fa2009-e492-459d-bace-fad9da6831bf",
 				DateTime:    time.Now(),
 				PatientName: "John Doe",
 				PatientID:   "c753a381-7642-4709-876f-57b16a5c6a6c",
 				DoctorName:  "Dr House",
 				DoctorID:    "f06244b9-97e5-4f1a-bae0-3b6da7a0b604",
 				Notes:       "some notes",
+				Canceled:    false,
+				Completed:   false,
 				CreatedBy:   "896d45e7-b544-41da-aa3f-f59a321fcdb9",
+				CreatedAt:   time.Now(),
+				UpdatedBy:   "896d45e7-b544-41da-aa3f-f59a321fcdb9",
+				UpdatedAt:   time.Now(),
 			},
 			nil,
 		},
@@ -33,9 +38,7 @@ func TestCreateAppointment(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			db, teardown := newTestDB(t)
 			defer teardown()
-
 			repo := storage.NewPgAppointmentRepository(db)
-
 			id, err := repo.Create(*tt.newAppointmt)
 			if !errors.Is(err, tt.wantError) {
 				t.Errorf("want %v; got %v", tt.wantError, err)
@@ -43,16 +46,99 @@ func TestCreateAppointment(t *testing.T) {
 			if (id == nil) && (tt.wantError == nil) {
 				t.Errorf("want %s; got id = nil", tt.newAppointmt.ID)
 			}
-			if id != nil {
-				a, _ := repo.FindByID(*id)
-				if (tt.newAppointmt.PatientName != a.PatientName) ||
-					(tt.newAppointmt.DoctorName != a.DoctorName) {
-					t.Errorf("want \n%v\n; got \n%v\n",
-						tt.newAppointmt.PatientName, a.PatientName)
-					t.Errorf("want \n%v\n; got \n%v\n",
-						tt.newAppointmt.DoctorName, a.DoctorName)
-				}
+			if tt.wantError == nil &&
+				id != nil &&
+				*id != tt.newAppointmt.ID {
+				t.Errorf("want \n%v\n; got \n%v\n",
+					tt.newAppointmt.ID, id)
 			}
 		})
 	}
+}
+
+func TestUpdateAppointment(t *testing.T) {
+	var tests = []struct {
+		name      string
+		appointmt appointment.Appointment
+		wantError error
+	}{
+		{"Valid Update",
+			appointment.Appointment{
+				ID:          "5e6f7cd1-d8d2-40cd-97a3-aca01a93bfde",
+				DateTime:    time.Date(2020, 9, 8, 12, 0, 0, 0, time.UTC),
+				PatientName: "John Doe",
+				PatientID:   "22070f56-5d52-43f0-9f59-5de61c1db506",
+				DoctorName:  "Dr House",
+				DoctorID:    "f06244b9-97e5-4f1a-bae0-3b6da7a0b604",
+				Notes:       "some new notes",
+				Canceled:    false,
+				Completed:   true,
+				CreatedBy:   "10b9ad06-e86d-4a85-acb1-d7e268d1f21a",
+				CreatedAt:   time.Date(2020, 9, 6, 12, 0, 0, 0, time.UTC),
+				UpdatedBy:   "10b9ad06-e86d-4a85-acb1-d7e268d1f21a",
+				UpdatedAt:   time.Date(2020, 9, 7, 12, 0, 0, 0, time.UTC),
+			},
+			nil,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			db, teardown := newTestDB(t)
+			defer teardown()
+			repo := storage.NewPgAppointmentRepository(db)
+			id, err := repo.Update(tt.appointmt)
+			if !errors.Is(err, tt.wantError) {
+				t.Errorf("want %v; got %v", tt.wantError, err)
+			}
+			if (id == nil) && (tt.wantError == nil) {
+				t.Errorf("want %s; got id = nil", tt.appointmt.ID)
+			}
+
+		})
+	}
+}
+
+func TestFindAppointmentByID(t *testing.T) {
+	var tests = []struct {
+		name      string
+		appointmt appointment.Appointment
+		wantError error
+	}{
+		{"Valid Update",
+			appointment.Appointment{
+				ID:          "5e6f7cd1-d8d2-40cd-97a3-aca01a93bfde",
+				DateTime:    time.Date(2020, 9, 7, 12, 0, 0, 0, time.UTC),
+				PatientName: "John Doe",
+				PatientID:   "22070f56-5d52-43f0-9f59-5de61c1db506",
+				DoctorName:  "Dr House",
+				DoctorID:    "f06244b9-97e5-4f1a-bae0-3b6da7a0b604",
+				Notes:       "some notes",
+				Canceled:    false,
+				Completed:   false,
+				CreatedBy:   "10b9ad06-e86d-4a85-acb1-d7e268d1f21a",
+				CreatedAt:   time.Date(2020, 9, 6, 12, 0, 0, 0, time.UTC),
+				UpdatedBy:   "10b9ad06-e86d-4a85-acb1-d7e268d1f21a",
+				UpdatedAt:   time.Date(2020, 9, 6, 12, 0, 0, 0, time.UTC),
+			},
+			nil,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			db, teardown := newTestDB(t)
+			defer teardown()
+			repo := storage.NewPgAppointmentRepository(db)
+			appointmt, err := repo.FindByID(tt.appointmt.ID)
+			if !errors.Is(err, tt.wantError) {
+				t.Errorf("want %v; got %v", tt.wantError, err)
+			}
+			if *appointmt != tt.appointmt {
+				t.Errorf("want %s; got id = nil", tt.appointmt.ID)
+			}
+
+		})
+	}
+
 }
