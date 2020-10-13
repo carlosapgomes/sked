f080d63115f08c6746b6162031ecb6bc58fad31c
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-10-13 14:17:59 -0300

    set app essential state

--- frontend/reactfrontend/src/App.js
+++ frontend/reactfrontend/src/App.js
@@ -1,14 +1,18 @@
-import React,{Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-	render(){
-		return (
-			<div className="App">
-			</div>
-		);
-	}
+  constructor(props) {
+    super(propos);
+    this.state = {
+      currentUser: null,
+      currentPatient: null,
+      currentDoctor: null,
+    };
+  }
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
