b42eef5a2f45d47bbd90590f1636ab143924d974
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-10-15 21:31:14 -0300

    initial loginHandler implementation

--- frontend/reactfrontend/src/Auth/Auth.js
+++ frontend/reactfrontend/src/Auth/Auth.js
@@ -3,7 +3,18 @@ import React, { useState } from "react";
 const Auth = (props) => {
   const [username, setUserName] = useState("");
   const [pword, setPword] = useState("");
-  const loginHandler = () => {};
+  const loginHandler = () => {
+    let ajax = new XMLHttpRequest();
+    ajax.open("POST", "https://myapp.local", true);
+    ajax.setRequestHeader("Content-type", "application/json");
+    ajax.send(JSON.Stringfy({ username: username, password: pword }));
+    ajax.onreadystatechange = () => {
+      if (ajax.readyState == 4 && ajax.status == 200) {
+        var data = ajax.responseText;
+        console.log(data);
+      }
+    };
+  };
   const logoutHandler = () => {};
   const cancelHandler = () => {};
 
@@ -16,11 +27,11 @@ const Auth = (props) => {
           onChange={(e) => setUserName(e.target.value)}
         />
         <input type="password" onChange={(e) => setPword(e.target.value)} />
-        <button onClick={this.loginHandler}>Ok</button>
-        <button onClick={this.cancelHandler}>Cancelar</button>
+        <button onClick={loginHandler}>Ok</button>
+        <button onClick={cancelHandler}>Cancelar</button>
       </div>
       <div id="logout" hidden={!props.loggedIn}>
-        <button onClick={this.logoutHandler}>Logout</button>
+        <button onClick={logoutHandler}>Logout</button>
       </div>
     </div>
   );
