c925c62ff8dbe6a5c3ca756d0c1f24f090baf711
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-10-21 19:32:03 -0300

    insert sample patients data

---
+++ dev/data/5-insertPatients.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+set -e
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "sked" <<'EOF'
+INSERT INTO patients (id, name, address, city, state, phones, created_by,
+	created_at, updated_by, updated_at) VALUES (
+'85f45ff9-d31c-4ff7-94ac-5afb5a1f0fcd',
+'Valid Patient',
+'Somewhere Street 42',
+'MainCity',
+'ST',
+'{"6544332135"}',
+'dcce1beb-aee6-4a4d-b724-94d470817323',
+'2019-06-23 17:25:00 UTC',
+'dcce1beb-aee6-4a4d-b724-94d470817323',
+'2019-06-23 17:25:00 UTC'
+);
+
+INSERT INTO patients (id, name, address, city, state, phones, created_by,
+	created_at, updated_by, updated_at) VALUES (
+'68b1d5e2-39dd-4713-8631-a08100383a0f',
+'Bob',
+'Somewhere Street 49',
+'MainCity',
+'ST',
+'{"6544334535"}',
+'dcce1beb-aee6-4a4d-b724-94d470817323',
+'2019-06-23 17:25:00 UTC',
+'dcce1beb-aee6-4a4d-b724-94d470817323',
+'2019-06-23 17:25:00 UTC'
+);
+
+INSERT INTO patients (id, name, address, city, state, phones, created_by,
+	created_at, updated_by, updated_at) VALUES (
+'dcce1beb-aee6-4a4d-b724-94d470817323',
+'Alice Jones',
+'Somewhere Street 46',
+'MainCity',
+'ST',
+'{"6544332135"}',
+'dcce1beb-aee6-4a4d-b724-94d470817323',
+'2019-06-23 17:25:00 UTC',
+'dcce1beb-aee6-4a4d-b724-94d470817323',
+'2019-06-23 17:25:00 UTC'
+);
+
+INSERT INTO patients (id, name, address, city, state, phones, created_by,
+	created_at, updated_by, updated_at) VALUES (
+'ecadbb28-14e6-4560-8574-809c6c54b9cb',
+'Barack Obama',
+'Somewhere Street 45',
+'MainCity',
+'ST',
+'{"6544332135"}',
+'dcce1beb-aee6-4a4d-b724-94d470817323',
+'2019-06-23 17:25:00 UTC',
+'dcce1beb-aee6-4a4d-b724-94d470817323',
+'2019-06-23 17:25:00 UTC'
+);
+
+INSERT INTO patients (id, name, address, city, state, phones, created_by,
+	created_at, updated_by, updated_at) VALUES (
+'ca16fc9d-df7b-4594-97e3-264432145b01',
+'SpongeBob Squarepants',
+'Somewhere Street 43',
+'MainCity',
+'ST',
+'{"65949340"}',
+'dcce1beb-aee6-4a4d-b724-94d470817323',
+'2019-06-23 17:25:00 UTC',
+'dcce1beb-aee6-4a4d-b724-94d470817323',
+'2019-06-23 17:25:00 UTC'
+);
+
+INSERT INTO patients (id, name, address, city, state, phones, created_by,
+	created_at, updated_by, updated_at) VALUES (
+'27f9802b-acb3-4852-bf97-c4ed4c3b3658',
+'Tim Berners-Lee',
+'Somewhere Street 43',
+'MainCity',
+'ST',
+'{"0323949324"}',
+'dcce1beb-aee6-4a4d-b724-94d470817323',
+'2019-06-23 17:25:00 UTC',
+'dcce1beb-aee6-4a4d-b724-94d470817323',
+'2019-06-23 17:25:00 UTC'
+);
+EOF
