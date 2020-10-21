19a6758170cd7c7da21344d3f23e9851366b7d26
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-10-21 19:38:00 -0300

    insert sample surgeries data

---
+++ dev/data/7-insertSurgeries.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+set -e
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "sked" <<'EOF'
+INSERT INTO surgeries (id, date_time, patient_name, patient_id, doctor_name,
+doctor_id, notes, proposed_surgery, canceled, done, created_by, 
+created_at, updated_by, updated_at) VALUES (
+'5e6f7cd1-d8d2-40cd-97a3-aca01a93bfde',
+'2020-09-07 12:00:00 UTC',
+'John Doe',
+'22070f56-5d52-43f0-9f59-5de61c1db506',
+'Dr House',
+'f06244b9-97e5-4f1a-bae0-3b6da7a0b604',
+'some notes',
+'saphenectomy',
+FALSE,
+FALSE,
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC',
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC'
+);
+
+
+INSERT INTO surgeries (id, date_time, patient_name, patient_id, doctor_name,
+doctor_id, notes, proposed_surgery, canceled, done, created_by, 
+created_at, updated_by, updated_at) VALUES (
+'7fef3c47-a01a-42a6-ac45-27a440596751',
+'2020-09-08 12:00:00 UTC',
+'John Doe',
+'22070f56-5d52-43f0-9f59-5de61c1db506',
+'Dr House',
+'f06244b9-97e5-4f1a-bae0-3b6da7a0b604',
+'some notes',
+'saphenectomy',
+FALSE,
+FALSE,
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC',
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC'
+);	
+
+INSERT INTO surgeries (id, date_time, patient_name, patient_id, doctor_name,
+doctor_id, notes, proposed_surgery, canceled, done, created_by, 
+created_at, updated_by, updated_at) VALUES (
+'00707378-7fd5-4cbe-92e1-ca93301eda49',
+'2020-09-09 12:00:00 UTC',
+'John Doe',
+'22070f56-5d52-43f0-9f59-5de61c1db506',
+'Dr House',
+'f06244b9-97e5-4f1a-bae0-3b6da7a0b604',
+'some notes',
+'saphenectomy',
+FALSE,
+FALSE,
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC',
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC'
+);	
+
+
+INSERT INTO surgeries (id, date_time, patient_name, patient_id, doctor_name,
+doctor_id, notes, proposed_surgery, canceled, done, created_by, 
+created_at, updated_by, updated_at) VALUES (
+'723e2fa0-70a9-4c20-89d9-b5f69405b772',
+'2020-09-06 12:00:00 UTC',
+'John Doe',
+'22070f56-5d52-43f0-9f59-5de61c1db506',
+'Dr House',
+'f06244b9-97e5-4f1a-bae0-3b6da7a0b604',
+'some notes',
+'saphenectomy',
+FALSE,
+FALSE,
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC',
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC'
+);
+	
+INSERT INTO surgeries (id, date_time, patient_name, patient_id, doctor_name,
+doctor_id, notes, proposed_surgery, canceled, done, created_by, 
+created_at, updated_by, updated_at) VALUES (
+'f49d46a0-6e44-4458-91bb-a3d6d9a4b156',
+'2020-09-11 12:00:00 UTC',
+'John Doe',
+'22070f56-5d52-43f0-9f59-5de61c1db506',
+'Dr House',
+'f06244b9-97e5-4f1a-bae0-3b6da7a0b604',
+'some notes',
+'saphenectomy',
+FALSE,
+FALSE,
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC',
+'10b9ad06-e86d-4a85-acb1-d7e268d1f21a',
+'2020-09-06 12:00:00 UTC'
+);	
+EOF
