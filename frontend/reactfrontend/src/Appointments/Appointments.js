4531e176baffe5ea6b8bcb72e74784f23935bed9
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-10-31 21:49:55 -0300

    add stubs for each page

---
+++ frontend/reactfrontend/src/Appointments/Appointments.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Appointments extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <h1>Appointments</h1>;
+  }
+}
---
+++ frontend/reactfrontend/src/Users/Users.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Users extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <h1>Users</h1>;
+  }
+}
