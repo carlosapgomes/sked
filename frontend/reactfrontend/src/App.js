1a21cc09afa8794eb4468f92e0582e23327d2c95
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-10-31 21:50:56 -0300

    add initial app layout and navigation

--- frontend/reactfrontend/src/App.js
+++ frontend/reactfrontend/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Auth from "./Auth/Auth";
+import Appointments from "./Appointments/Appointments";
+import Surgeries from "./Surgeries/Surgeries";
+import Patients from "./Patients/Patients";
+import Users from "./Users/Users";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -53,42 +58,69 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <header>Sked(uler)</header>
-        <nav>
-          <div id="logout" hidden={!this.state.loggedIn}>
-            <button
-              onClick={() => {
-                this.logoutHandler();
-              }}
-            >
-              Logout
-            </button>
-          </div>
-        </nav>
-        <main>
-          {!this.state.loggedIn ? (
-            <Auth
-              updateLogin={(s) => this.updateLogin(s)}
-              updateUser={(u) => this.updateUser(u)}
-              loggedIn={this.state.loggedIn}
-            />
-          ) : (
-            <div>
-              <h1>Welcome to sked {this.state.currentUser.name}</h1>
-              <button
-                onClick={() => {
-                  this.getResult();
-                }}
-              >
-                Query
-              </button>
-              <p>{this.state.result}</p>
-            </div>
-          )}
-        </main>
-        <footer>&#169; CG - 2020</footer>
-      </div>
+      <Router>
+        <div className="App">
+          <header>
+            Sked(uler)
+            <nav className="Navigation">
+              <div id="logout" hidden={!this.state.loggedIn}>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/Appointments">Appointments</Link>
+                  </li>
+                  <li>
+                    <Link to="/Surgeries">Surgeries</Link>
+                  </li>
+                  <li>
+                    <Link to="/Patients">Patients</Link>
+                  </li>
+                  <li>
+                    <Link to="/Users">Users</Link>
+                  </li>
+                </ul>
+                <button
+                  onClick={() => {
+                    this.logoutHandler();
+                  }}
+                >
+                  Logout
+                </button>
+              </div>
+            </nav>
+          </header>
+          <main>
+            {!this.state.loggedIn ? (
+              <Auth
+                updateLogin={(s) => this.updateLogin(s)}
+                updateUser={(u) => this.updateUser(u)}
+                loggedIn={this.state.loggedIn}
+              />
+            ) : (
+              <Switch>
+                <Route path="/" exact>
+                  <h1>Skeduler</h1>
+                </Route>
+                <Route path="/Appointments">
+                  <Appointments />
+                </Route>
+                <Route path="/Surgeries">
+                  <Surgeries />
+                </Route>
+                <Route path="/Patients">
+                  <Patients />
+                </Route>
+                <Route path="/Users">
+                  <Users />
+                </Route>
+              </Switch>
+            )}
+          </main>
+          <footer>&#169; CG - 2020</footer>
+        </div>
+      </Router>
     );
   }
 }
