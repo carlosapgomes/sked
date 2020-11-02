9c12fb89f405c7999b3eb7065986458333a80fd2
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-11-02 13:31:16 -0300

    implement save appointments

--- frontend/reactfrontend/src/Appointments/Appointments.js
+++ frontend/reactfrontend/src/Appointments/Appointments.js
@@ -1,35 +1,92 @@
 import React, { Component } from "react";
+import dayjs from "dayjs";
 
 export default class Appointments extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      initialSelectedValue: "selectAnOption",
+      selectedDoctorValue: "selectAnOption",
       selectedDoctor: null,
       searchField: "",
       patientSearchResult: [],
+      selectedPatientValue: "selectAnOption",
       selectedPatient: null,
       selectedDate: "",
+      selectedTime: "08:00",
+      notes: "",
     };
   }
   resetInitialState() {
     this.setState({
-      initialSelectedValue: "selectAnOption",
+      selectedDoctorValue: "selectAnOption",
       selectedDoctor: null,
       searchField: "",
       patientSearchResult: [],
+      selectedPatientValue: "selectAnOption",
       selectedPatient: null,
       selectedDate: "",
+      selectedTime: "08:00",
+      notes: "",
     });
   }
   clearForm() {
     this.resetInitialState();
   }
-  saveAppointment() {}
+  saveAppointment() {
+    if (
+      this.state.selectedDoctor == null ||
+      this.state.selectedPatient === null ||
+      this.state.selectedDate === "" ||
+      this.state.selectedTime === ""
+    ) {
+      window.alert("Fill all fields");
+      return;
+    }
+    let dateTime = dayjs(
+      this.state.selectedDate + " " + this.state.selectedTime
+    );
+    let appointmt = {
+      dateTime: dateTime.format(),
+      patientName: this.state.selectedPatient.name,
+      patientID: this.state.selectedPatient.id,
+      doctorName: this.state.selectedDoctor.name,
+      doctorID: this.state.selectedDoctor.id,
+      notes: this.state.notes,
+      createdBy: this.props.currentUser.uid,
+    };
+    console.log(appointmt, null, 2);
+    let ajax = new XMLHttpRequest();
+    let url = "https://dev.local/api/appointments";
+    ajax.open("POST", url, true);
+    ajax.withCredentials = true;
+    ajax.setRequestHeader("Content-type", "application/json");
+    ajax.send(JSON.stringify(appointmt));
+    ajax.onreadystatechange = () => {
+      if (ajax.readyState === 4 && ajax.status === 200) {
+        window.alert("Appointment saved");
+        this.clearForm();
+      }
+    };
+  }
+  setSelectedDoctor(e) {
+    let idx = e.target.selectedIndex - 1;
+    if (idx >= 0) {
+      this.setState({
+        selectedDoctorValue: this.props.doctors[idx].id,
+        selectedDoctor: { ...this.props.doctors[idx] },
+      });
+    }
+  }
   setSelectedPatient(e) {
-    let idx = e.target.selectedIndex;
+    let idx = e.target.selectedIndex - 1;
     this.setState({
+      selectedPatientValue: this.state.patientSearchResult[idx].id,
       selectedPatient: { ...this.state.patientSearchResult[idx] },
+    });
+  }
+  setTime(e) {
+    this.setState({
+      selectedTime: e.target.value,
     });
   }
   setDate(e) {
@@ -37,13 +94,10 @@ export default class Appointments extends Component {
       selectedDate: e.target.value,
     });
   }
-  setSelectedDoctor(e) {
-    let idx = e.target.selectedIndex - 1;
-    if (idx >= 0) {
-      this.setState({
-        selectedDoctor: { ...this.props.doctors[idx] },
-      });
-    }
+  setNotes(s) {
+    this.setState({
+      notes: s,
+    });
   }
   updateSearchField(s) {
     this.setState({
@@ -93,7 +147,7 @@ export default class Appointments extends Component {
                 onChange={(e) => {
                   this.setSelectedDoctor(e);
                 }}
-                value={this.state.initialSelectedValue}
+                value={this.state.selectedDoctorValue}
               >
                 <option
                   hidden
@@ -115,16 +169,27 @@ export default class Appointments extends Component {
               </select>
             </div>
           ) : null}
-          <label htmlFor="apptmtdate">Data: </label>
-          <input
-            type="date"
-            id="apptmtdate"
-            name="apptmtdate"
-            value={this.state.selectedDate}
-            onChange={(e) => {
-              this.setDate(e);
-            }}
-          />
+          <label htmlFor="apptmtdatetime">Date/Time: </label>
+          <div id="apptmtdatetime">
+            <input
+              type="date"
+              id="apptmtdate"
+              name="apptmtdate"
+              value={this.state.selectedDate}
+              onChange={(e) => {
+                this.setDate(e);
+              }}
+            />
+            <input
+              type="time"
+              value={this.state.selectedTime}
+              name="time"
+              id="time"
+              onChange={(e) => {
+                this.setTime(e);
+              }}
+            />
+          </div>
           <label htmlFor="pctsearch">Patient: </label>
           <input
             type="text"
@@ -146,11 +211,21 @@ export default class Appointments extends Component {
             <select
               name="searchresult"
               id="searchresult"
-              size="4"
               onChange={(e) => {
                 this.setSelectedPatient(e);
               }}
+              value={this.state.selectedPatientValue}
             >
+              <option
+                hidden
+                disabled
+                defaultValue
+                value="selectAnOption"
+                style={{ display: "none" }}
+              >
+                {" "}
+                -- select an option --{" "}
+              </option>
               {this.state.patientSearchResult.map((p) => {
                 return (
                   <option key={p.id} value={p.id}>
@@ -159,6 +234,17 @@ export default class Appointments extends Component {
                 );
               })}
             </select>
+          </div>
+          <div>
+            <label htmlFor="notes">Notes: </label>
+            <textarea
+              name="notes"
+              id="notes"
+              onChange={(e) => {
+                this.setNotes(e.target.value);
+              }}
+              value={this.state.notes}
+            ></textarea>
           </div>
           <div>
             <button
