282e5a624f68082d7d4a60ece1772802b7858a0a
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-11-16 21:19:05 -0300

    test(appointments): add test for finding by month
    test endpoint code for finding appointments by month/year

--- backend/internal/web/appointmentHandlers_test.go
+++ backend/internal/web/appointmentHandlers_test.go
@@ -210,6 +210,75 @@ func TestFindAppointmentByPatientID(t *testing.T) {
 		})
 	}
 }
+func TestFindAppointmentsByMonth(t *testing.T) {
+	userSvc := services.NewUserService(mocks.NewUserRepo())
+	handlers := web.New(
+		log.New(ioutil.Discard, "", 0),
+		log.New(ioutil.Discard, "", 0),
+		&web.CkProps{
+			Name:     "sid",
+			HTTPOnly: false,
+			Secure:   false,
+		},
+		mocks.NewSessionSvc(),
+		userSvc,
+		nil,
+		mocks.NewTokenMockSvc(),
+		nil,
+		services.NewAppointmentService(mocks.NewAppointmentRepo(), userSvc),
+		nil,
+	)
+	ts := newTestServer(t, handlers.Routes())
+	var tests = []struct {
+		name     string
+		month    int
+		year     int
+		wantSize int
+		wantCode int
+	}{
+		{
+			"Valid Search",
+			9,
+			2020,
+			6,
+			http.StatusOK,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			path := ts.URL + "/appointments?month=" + strconv.Itoa(tt.month) +
+				"&year=" + strconv.Itoa(tt.year)
+			req, _ := http.NewRequest(http.MethodGet, path, nil)
+			cookie := &http.Cookie{
+				Name:  "sid",
+				Value: "167ced64-af16-45d2-bb08-e35233c04ad1",
+			}
+			req.AddCookie(cookie)
+			rs, err := ts.Client().Do(req)
+			if err != nil {
+				t.Fatal(err)
+			}
+			code := rs.StatusCode
+			if code != tt.wantCode {
+				t.Errorf("want %d; got %d", tt.wantCode, code)
+			}
+			var response []appointment.Appointment
+			if tt.wantCode == http.StatusOK {
+				defer rs.Body.Close()
+				respBody, _ := ioutil.ReadAll(rs.Body)
+				//t.Logf("%s\n", respBody)
+				err = json.Unmarshal(respBody, &response)
+				if err != nil {
+					t.Error("bad response body")
+				}
+			}
+			if len(response) != tt.wantSize {
+				t.Errorf("Want response size %v, but got %v\n", tt.wantSize,
+					len(response))
+			}
+		})
+	}
+}
 func TestFindAppointmentByDate(t *testing.T) {
 	userSvc := services.NewUserService(mocks.NewUserRepo())
 	handlers := web.New(
