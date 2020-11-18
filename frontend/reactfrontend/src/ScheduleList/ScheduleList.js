d4b234a412c5bdf0123a31c1e1128556b0c7b544
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-11-18 14:20:26 -0300

    fix(schedules list): use simpler month/year picker
    firefox does not support <input type="month" /> yet

--- frontend/reactfrontend/src/ScheduleList/ScheduleList.js
+++ frontend/reactfrontend/src/ScheduleList/ScheduleList.js
@@ -7,24 +7,27 @@ export default class ScheduleList extends Component {
 
     this.state = {
       currentMonth: "",
+      currentYear: "",
       days: undefined,
       appointments: undefined,
       surgeries: undefined,
     };
   }
   componentDidMount() {
-    let m = dayjs().format("YYYY-MM");
+    const m = String(dayjs().month());
+    const y = String(dayjs().year());
     this.setState({
       currentMonth: m,
+      currentYear: y,
     });
-    const nDays = new Date(m.substr(0, 4), m.substr(5, 2), 0).getDate();
-    let days = [];
-    for (let i = 1; i <= nDays; i++) {
-      days.push(<li key={i.toString()}>{i}</li>);
-    }
-    this.setState({
-      days: [...days],
-    });
+    //const nDays = new Date(m.substr(0, 4), m.substr(5, 2), 0).getDate();
+    //let days = [];
+    //for (let i = 1; i <= nDays; i++) {
+    //days.push(<li key={i.toString()}>{i}</li>);
+    //}
+    //this.setState({
+    //days: [...days],
+    //});
   }
   getAllAppointmentsInAMonth(m, y) {
     let ajax = new XMLHttpRequest();
@@ -89,13 +92,25 @@ export default class ScheduleList extends Component {
     ) {
       return;
     }
-    const nDays = new Date(y, m, 0).getDate();
-    let days = [];
-    for (let i = 1; i <= nDays; i++) {
-      days.push(<li key={i.toString()}>{i}</li>);
-    }
+    //const nDays = new Date(y, m, 0).getDate();
+    let daysOfMonth = [];
+    //for (let i = 1; i <= nDays; i++) {
+    //daysOfMonth.push(
+    //<li key={i.toString()}>
+    //{this.state.appointments.map((e) => {
+    //if (dayjs(e.date_time).date() === i) {
+    //return (
+    //<div>
+    //{e.doctor_name}: {e.patient_name}
+    //</div>
+    //);
+    //}
+    //})}
+    //</li>
+    //);
+    //}
     this.setState({
-      days: [...days],
+      days: [...daysOfMonth],
     });
   }
   setCurrentMonth(m) {
@@ -104,23 +119,66 @@ export default class ScheduleList extends Component {
       appointments: undefined,
       surgeries: undefined,
     });
-    let year = m.substr(0, 4);
-    let month = m.substr(5, 2);
-    this.getAllSurgeriesInAMonth(month, year);
-    this.getAllAppointmentsInAMonth(month, year);
+    console.log(this.state.currentMonth);
+    console.log(this.state.currentYear);
+
+    //let year = m.substr(0, 4);
+    //let month = m.substr(5, 2);
+    //this.getAllSurgeriesInAMonth(month, year);
+    //this.getAllAppointmentsInAMonth(month, year);
+  }
+  setCurrentYear(y) {
+    this.setState({
+      currentYear: y,
+      appointments: undefined,
+      surgeries: undefined,
+    });
+    console.log(this.state.currentMonth);
+    console.log(this.state.currentYear);
   }
   render() {
     return (
       <div>
-        <input
-          type="month"
-          value={this.state.currentMonth}
-          name="month"
-          id="month"
-          onChange={(e) => {
-            this.setCurrentMonth(e.target.value);
-          }}
-        />
+        <span>
+          <label for="month">Month: </label>
+          <select
+            id="month"
+            name="month"
+            value={this.state.currentMonth}
+            onChange={(e) => {
+              this.setCurrentMonth(e.target.value);
+            }}
+          >
+            <option value="01">01</option>
+            <option value="02">02</option>
+            <option value="03">03</option>
+            <option value="04">04</option>
+            <option value="05">05</option>
+            <option value="06">06</option>
+            <option value="07">07</option>
+            <option value="08">08</option>
+            <option value="09">09</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+          </select>
+        </span>
+        {"    "}
+        {"    "}
+        <span>
+          <label for="year">Year: </label>
+          <select
+            id="year"
+            name="year"
+            value={this.state.currentYear}
+            onChange={(e) => {
+              this.setCurrentYear(e.target.value);
+            }}
+          >
+            <option value="2020">2020</option>
+            <option value="2021">2021</option>
+          </select>
+        </span>
         <div>
           <ul className={cl.ul}>{this.state.days}</ul>
         </div>
