03aa93f7209ccf86a735de91ef2933891e195557
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-12-09 20:06:06 -0300

    fix(dev): remove reduntant file

--- dev/startPg.sh
+++
@@ -1,9 +0,0 @@
-DATADIR=`mktemp -d /tmp/skedPgData.XXXXXX` || exit 1
-docker run -d \
-        --name pgDevEnv \
-        -e POSTGRES_PASSWORD=pgDevEnv \
-        -v ${DATADIR}/:/var/lib/postgresql/data \
-        -v ${PWD}/data:/docker-entrypoint-initdb.d/ \
-        -p 54320:5432 \
-        postgres
-
