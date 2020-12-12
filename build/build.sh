8b564222fc1117268daa0b7e05e02f0137271c1b
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-12-12 16:17:13 -0300

    feat(build): improve build.sh script

--- build/build.sh
+++ build/build.sh
@@ -1,20 +1,34 @@
 #!/bin/sh
 
-echo "Building the backend binary"
-SKEDDIR=`$PWD/provisioning/roles/backend/files/opt/skedbackend` || exit 1
+echo "Preparing folders"
+if [ ! -d  "$PWD/provisioning/roles/backend/files/opt/skedbackend" ]; then
+    echo "making folders"
+    mkdir -p "$PWD/provisioning/roles/backend/files/opt/skedbackend" || exit 1
+fi
+SKEDDIR="$PWD/provisioning/roles/backend/files/opt/skedbackend" || exit 1
+echo "changing dir to ../backend"
 cd ../backend
-/usr/local/go/bin/go build -o "$SKEDDIR/skedbackend" cmd/main.go;
+echo "Building skedbackend"
+/usr/local/go/bin/go build -o "$SKEDDIR/skedbackend" cmd/main.go
 cd ../build
 echo "Moving assets to the temporary folder"
-mkdir "$SKEDDIR/templates" || exit 1;
-cp ../backend/internal/web/templates/* "$SKEDDIR/templates" || exit 1;
+if [ ! -d  "$SKEDDIR/templates" ]; then
+    mkdir "$SKEDDIR/templates" || exit 1
+fi
+cp ../backend/internal/web/templates/* "$SKEDDIR/templates" || exit 1
 echo
 echo "Building the frontend"
 cd ../frontend/reactfrontend
 yarn build
-cp -r public/* ../../build/frontend/files/var/www/sked/html/ || exit 1;
-cd ../../build
-
+echo "cd to project root"
+cd ../../
+if [ ! -d "$PWD/build/provisioning/roles/frontend/files/var/www/sked/html" ]; then
+    echo "making frontend folders"
+    mkdir -p "$PWD/build/provisioning/roles/frontend/files/var/www/sked/html" || exit 1
+fi
+echo "current folder: $PWD"
+cp -R "$PWD/frontend/reactfrontend/build/." "$PWD/build/provisioning/roles/frontend/files/var/www/sked/html" || exit 1
+cd build
 echo
 echo "running ansible..."
 
