8a954820054093d0280cc1a65c666a82a7fc3a0f
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-12-15 13:49:25 -0300

    test(frontend): fix i18n init resources

--- frontend/reactfrontend/src/i18nForTests.js
+++ frontend/reactfrontend/src/i18nForTests.js
@@ -1,19 +1,19 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "../public/locales/en/translation.json";
-
+import pt from "../public/locales/pt/translation.json";
 i18n.use(initReactI18next).init({
-  lng: "en",
+  lng: "pt-br",
   fallbackLng: "en",
   ins: ["translations"],
   defaultNS: "translations",
-
   debug: true,
   interpolation: {
     escapeValue: false,
   },
   resources: {
-    en,
+    en: { translations: en },
+    "pt-BR": { translations: pt },
   },
 });
 export default i18n;
