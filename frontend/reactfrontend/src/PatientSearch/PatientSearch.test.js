196b50027720cb90f09078025b20174c6f7a2db5
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-12-15 15:58:05 -0300

    test(frontend): add i18n to tests

--- frontend/reactfrontend/src/PatientSearch/PatientSearch.test.js
+++ frontend/reactfrontend/src/PatientSearch/PatientSearch.test.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import PatientSearch from "./PatientSearch";
+import { I18nextProvider } from "react-i18next";
+import i18n from "../i18nForTests";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  render(<PatientSearch currentPatient={null} />, div);
+  render(
+    <I18nextProvider i18n={i18n}>
+      <PatientSearch currentPatient={null} />
+    </I18nextProvider>,
+    div
+  );
 });
--- frontend/reactfrontend/src/Patients/Patients.test.js
+++ frontend/reactfrontend/src/Patients/Patients.test.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Patients from "./Patients";
+import { I18nextProvider } from "react-i18next";
+import i18n from "../i18nForTests";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  render(<Patients currentPatient={null} />, div);
+  render(
+    <I18nextProvider i18n={i18n}>
+      <Patients currentPatient={null} />
+    </I18nextProvider>,
+    div
+  );
 });
