ef90cec7fdccde2fe07fe271474c59389d019753
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2020-12-30 18:29:27 -0300

    feat(frontend): add calendar labels and fix styles

--- frontend/reactfrontend/src/Calendar/Calendar.js
+++ frontend/reactfrontend/src/Calendar/Calendar.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { withTranslation } from "react-i18next";
+import "./Calendar.css";
 
-export default class Calendar extends Component {
+class Calendar extends Component {
   constructor(props) {
     super(props);
 
@@ -8,11 +10,28 @@ export default class Calendar extends Component {
   }
 
   render() {
+    const { t } = this.props;
     return (
       <div className="Calendar">
-        <div className="Labels"></div>
-        <div className="dates"></div>
+        <div className="Labels">
+          <span>{t("Sunday")}</span>
+          <span>{t("Monday")}</span>
+          <span>{t("Tuesday")}</span>
+          <span>{t("Wednesday")}</span>
+          <span>{t("Thursday")}</span>
+          <span>{t("Friday")}</span>
+          <span>{t("Saturday")}</span>
+        </div>
+        <div className="Dates">
+          {this.props.view.map((w) => {
+            let i = 0;
+            return w.map((d) => {
+              return <span key={i++}>{d > 0 ? d : ""}</span>;
+            });
+          })}
+        </div>
       </div>
     );
   }
 }
+export default withTranslation()(Calendar);
